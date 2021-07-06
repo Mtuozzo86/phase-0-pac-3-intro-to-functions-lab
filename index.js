4aba8378871cfd6f185e21013581db11c851ed39
Mtuozzo86 <Mtuozzo86@gmail.com>
2021-07-06 17:00:41 -0400

    commit tree c56740f9d78e47605a5ca0bbf0f886cfbadfd13e

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-function shout(string) {
-    return string.toUpperCase();
+// function shout(string) {
+//     return string.toUpperCase();
+// }
+
+// function whisper(string) {
+//     return string.toLowerCase();
+// }
+
+// function logShout(string) {
+//     console.log(string.toUpperCase())
+// }
+// logShout(string);
+
+// function logWhisper(string) {
+//     console.log(string.toLowerCase())
+// }
+// logWhisper(string)
+
+// function sayHiToGrandma(string) {
+//     if(string.toLowerCase() === string)
+//     return "I can't hear you!"
+
+//     if(string.toUpperCase() === string)
+//     return "YES INDEED!"
+
+//     if(string === "I love you, Grandma.")
+//     return "I love you, too."
+// }
+
+function shout(string){
+    return string.toUpperCase()
 }
 
-function whisper(string) {
-    return string.toLowerCase();
+function whisper(string){
+    return string.toLowerCase()
 }
 
-function logShout(string) {
-    console.log(string.toUpperCase())
-}
-logShout(string);
-
-function logWhisper(string) {
-    console.log(string.toLowerCase())
-}
-logWhisper(string)
-
-function sayHiToGrandma(string) {
-    if(string.toLowerCase() === string)
-    return "I can't hear you!"
-
-    if(string.toUpperCase() === string)
-    return "YES INDEED!"
-
-    if(string === "I love you, Grandma.")
-    return "I love you, too."
+function logShout(string){
+ console.log().toUpperCase()
 }
